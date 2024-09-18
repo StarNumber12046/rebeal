281da918db747fd0d2cfcb7d9a387039bdf3e4ef
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-09-18 19:40:55 +0200

    fixed build

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "expo-build-properties";
 export default {
   expo: {
     name: "rebeal",
-    slug: "ReBeal.",
+    slug: "slug",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
