ec4d676be2ee1a528f25c52137aee7642bd624f6
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-09-22 17:17:24 +0200

    fixed text spacing

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     zIndex: 99,
   },
   text: {
-    fontSize: 28,
+    fontSize: 24,
     fontWeight: "bold",
     alignSelf: "center", // Center each text element vertically
     color: "white",
